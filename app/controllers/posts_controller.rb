b7d66dbf204db61a7d3913ab50d5dfe76632944c
github username <github email address>
2016-10-24 15:56:18 -0400

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,7 @@ class PostsController < ApplicationController
   before_action :set_post!, only: [:show, :edit, :update]
 
   def show
+    set_post!
   end
 
   def edit
@@ -10,7 +11,13 @@ class PostsController < ApplicationController
   def update
     @post.update(post_params)
 
-    redirect_to post_path(@post)
+    if @post.valid?
+      @post.save
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
+
   end
 
   private
